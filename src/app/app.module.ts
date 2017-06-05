e3b4e218c295224188a5b9b3891a80c6d81a6165
Laury Lafage <lowrey97x@hotmail.fr>
2017-06-05 18:15:36 +0200

    Add materializecss.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MaterializeModule } from 'angular2-materialize';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterializeModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
