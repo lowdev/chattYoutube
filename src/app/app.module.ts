a684f9dd7cc0c538e6458f600233c99bc5c276f2
Laury Lafage <lowrey97x@hotmail.fr>
2017-06-17 14:04:23 +0200

    Add router.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MaterializeModule } from 'angular2-materialize';
@@ -8,6 +9,15 @@ import { VideosListComponent } from './videos-list/videos-list.component';
 import { VideosSearchBarComponent } from './videos-search-bar/videos-search-bar.component';
 
 import { APP_SERVICES } from './service';
+
+const appRoutes: Routes = [
+  //{ path: 'video/:id', component: VideoComponent },
+  {
+    path: '',
+    component: VideosListComponent
+  },
+  //{ path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
@@ -17,6 +27,7 @@ import { APP_SERVICES } from './service';
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     HttpModule,
     MaterializeModule
   ],
