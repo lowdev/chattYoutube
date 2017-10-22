1e4667e77c573cd59b07367afbd4abd2bf4dcd0f
Laury Lafage <lowrey97x@hotmail.fr>
2017-10-22 10:24:08 +0200

    Change password youtube

--- src/app/service/constants.ts
+++ src/app/service/constants.ts
@@ -1,2 +1,2 @@
-export const YOUTUBE_API_KEY: string = 'AIzaSyChAK118xdXIgMgwPkHXgsEwFeilme21nU';
+export const YOUTUBE_API_KEY: string = 'qYiUGtoFsIfwZns_Xfz7kZsO';
 export const CLIENT_ID: string = '971861197531-hm7solf3slsdjc4omsfti4jbcbe625hs';
